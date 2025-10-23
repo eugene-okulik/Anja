2c9bc92fa349e7459c8b64cfc114788944b57fa2
Anja <anjazxcvbnm@gmail.com>
2025-10-23 15:44:16 +0300

    fixing bugs in hw

--- homework/lesson5_hw/less5_task_hw.py
+++ homework/lesson5_hw/less5_task_hw.py
@@ -1,38 +1,26 @@
+'''homework_5'''
 #task_1
 person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
 name, last_name, city, phone, country = person
 print(name, last_name, city, phone, country)
 
 #task_2
-#option_1
-a = 'результат операции: 42'.index('42')
-b = 'результат операции: 514'.index('514')
-c = 'результат работы программы: 9'.index('9')
-print(
-    ((int('результат операции: 42'[a:])) + 10),
-    ((int('результат операции: 514'[b:])) + 10),
-    ((int('результат работы программы: 9'[c:])) + 10)
-)
-
-#option_2
 a = 'результат операции: 42'
 b = 'результат операции: 514'
 c = 'результат работы программы: 9'
-aid=a.index('42')
-bid=b.index('514')
-cid=c.index('9')
-print(
-    ((int(a[aid:])) + 10),
-    ((int(b[bid:])) + 10),
-    ((int(c[cid:])) + 10)
-)
+
+ind_a = a.index(':')
+ind_b = b.index(':')
+ind_c = c.index(':')
+
+int_a = int(a[ind_a + 1:].strip())
+int_b = int(b[ind_b + 1:].strip())
+int_c = int(c[ind_c + 1:].strip())
+
+print(int_a + 10, int_b + 10, int_c + 10)
 
 #task_3
 students=['Ivanov', 'Petrov', 'Sidorov']
 subjects=['math', 'biology', 'geography']
 sb=','.join(subjects)
-#option_1 - string-format
-print('Students {0} study these subjects: {1}'.format(','.join(students), sb))
-
-#option_2 - f-string
 print(f'Students {','.join(students)} study these subjects: {sb}')
