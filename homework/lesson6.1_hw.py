2e404f9199fd8769cad7d41bc9db368030533d0b
Anja <anjazxcvbnm@gmail.com>
2026-01-14 17:02:20 +0300

    error correction

--- homework/lesson6.1_hw.py
+++ homework/lesson6.1_hw.py
@@ -1,5 +1,7 @@
 # task_1
-text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+text = ('Etiam tincidunt neque erat, quis molestie enim '
+        'imperdiet vel. Integer urna nisl, facilisis vitae semper at, '
+        'dignissim vitae libero')
 words = text.split()
 result_words = []
 for word in words:
